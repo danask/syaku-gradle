1d0150895f6c85cca52374814132aba84fb3466f
최석균 <syaku@naver.com>
2016-03-31 13:48:37 +0900

    완료.

---
+++ src/main/java/org/syaku/HelloWorld.java
@@ -0,0 +1,13 @@
+package org.syaku;
+
+/**
+ * @author Seok Kyun. Choi. 최석균 (Syaku)
+ * @site http://syaku.tistory.com
+ * @since 16. 3. 31.
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+	}
+}
